3786479413a69d58caefb0f636020898a4c551a8
Timothy Henry <44324809+tmhvrml@users.noreply.github.com>
2022-03-30 13:04:34 -0400

    rev1

--- week4/team_class/include/functions.php
+++ week4/team_class/include/functions.php
@@ -1,10 +1,12 @@
 <?php
 
-/**
- * A method to check if a Post request has been made.
- *    
- * @return boolean
- */
+
 function isPostRequest() {
     return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
 }
+
+function isGetRequest() {
+    return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'GET' && !empty($_GET) );
+}
+
+?>
