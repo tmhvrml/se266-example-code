2e48629d7b1354fc6026445cccbec0ff8fcef31c
tmhvrml <thenry@neit.edu>
2022-04-12 09:06:59 -0400

    driver

--- week3/Student.php
+++ week3/Student.php
@@ -32,18 +32,4 @@ class Student extends Person
 
 } // end student
 
-// The code below runs everytime this class loads and 
-// should be commented out after testing.
-// $someStudent = new Student('Mickey', 'Mouse', '123456789', 3.5);
-// // var_dump ($s);
-
-// // $p = new Person ('Donald', 'Duck');
-
-// echo Student::getObjectCount() . "<br />";
-// echo $someStudent->getFullName() . "<br />";
-// echo $someStudent->getStudentSSN() . "<br />";
-echo "<hr />";
-// echo $p->getFullName() . "<br />";
-// echo Student::getObjectCount() . "<br />";
-
 ?>
--- week3/Worker.php
+++ week3/Worker.php
@@ -25,18 +25,5 @@ class Worker extends Person
 
 } // end worker class
 
-// The code below runs everytime this class loads and 
-// should be commented out after testing.
-// $someWorker = new Worker('Mickey', 'Mouse', '123456789', 3.5);
-// // var_dump ($s);
-
-// // $p = new Person ('Donald', 'Duck');
-
-
-// echo $someWorker->getFullName() . "<br />";
-// echo $someWorker->getJob() . "<br />";
-// echo "<hr />";
-// echo $p->getFullName() . "<br />";
-// echo Student::getObjectCount() . "<br />";
 
 ?>
