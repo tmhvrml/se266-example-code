30557a990843327941dc33403595182b737f1601
tmhvrml <thenry@neit.edu>
2022-04-25 13:28:59 -0400

    wk6

--- week6/file.php
+++ week6/file.php
@@ -1,29 +1,33 @@
 <?php
 
-// $names = file('names.txt');
-// echo $names[0];
-//var_dump ($names); // shows an array
+$names = file('names.txt');
+echo $names[0]. "*** <br />";
+var_dump ($names); // shows an array
 
 
+echo "<p> </p>";
+$file = fopen ('names.txt', 'rb');
+while (!feof($file)) {
+   $name = fgets ($file);
+   echo $name . "*** <br />";
+}
 
-// $file = fopen ('names.txt', 'rb');
-// while (!feof($file)) {
-//    $name = fgets ($file);
-//    echo $name . "*** <br />";
-// }
+
 define ("MAX_SCHOOLS", 10);
+echo "<p> </p>";
 
 if (!file_exists("upload/schools.csv")) {
-   echo "File does not exist";
+   echo "<p>File does not exist</p>";
    exit;
 }
 $file = fopen ('upload/schools.csv', 'rb');
 $i = 0;
-while (!feof($file) && $i < MAX_SCHOOLS ) {
+while (!feof($file) && $i < MAX_SCHOOLS )
+ {
    
-   $school = fgetcsv($file);
+  $school = fgetcsv($file);
    
-   echo ($school[1]) . "<br />";
+   echo $school[0] . "  ---  " . $school[1] . "<br />";
    $i++;
 }
 
