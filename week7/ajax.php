3383a579207481651e81c17ce7df09f921649a42
tmhvrml <thenry@neit.edu>
2022-05-02 10:19:56 -0400

    wk7

--- week7/ajax.php
+++ week7/ajax.php
@@ -1,18 +1,26 @@
 <?php
 
+// Check to see if content type was set and trim leading/trailing spaces
 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
-if ($contentType === "application/json") {
-  //Receive the RAW post data.
+// Check to see if we were sent a JSON file
+if ($contentType === "application/json") 
+{
+  //Receive the RAW post data into $content and trim trailing empty lines
   $content = trim(file_get_contents("php://input"));
  
+  // Decode JSON file into an associative array
   $decoded = json_decode($content, true);
 
   //If json_decode failed, the JSON is invalid.
-  if( is_array($decoded)) {
+  if( is_array($decoded)) 
+  {
+      // Display User Name from JSON file
       echo json_encode($decoded['user_name1']);
-  } else {
-    // Send error back to user.
+      
+  } else 
+  {
+    echo "<h2>Invalid JSON File sent!</h2>";
   }
 }
 
