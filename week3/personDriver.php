266d341a515b606d06df8df148e525789e5c8387
tmhvrml <thenry@neit.edu>
2022-10-04 15:04:45 -0400

    model folder

--- week3/personDriver.php
+++ week3/personDriver.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once("Student.php");
-include_once("Worker.php");
+include_once("models/Student.php");
+include_once("models/Worker.php");
 
 // ********************************************************
 // Testing Person Class
--- week3/polyPerson.php
+++ week3/polyPerson.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once("./Student.php");
-include_once("./Worker.php");
+include_once("./models/Student.php");
+include_once("./models/Worker.php");
 
 function whoWeAre($listOfPeople)
 {
