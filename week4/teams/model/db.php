0a52ffab6c759a690fb0ea72edc7c5296eadf1d4
tmhvrml <thenry@neit.edu>
2022-10-17 13:24:05 -0400

    mysql

--- week4/teams/model/db.php
+++ week4/teams/model/db.php
@@ -3,7 +3,7 @@
 
 $ini = parse_ini_file( __DIR__ . '/dbconfig.ini');
 
-$db = new PDO(  "pgsql:host=" . $ini['servername'] . 
+$db = new PDO(  "mysql:host=" . $ini['servername'] . 
                 ";port=" . $ini['port'] . 
                 ";dbname=" . $ini['dbname'], 
                 $ini['username'], 
