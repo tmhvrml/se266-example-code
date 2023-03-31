9bd52a938fa903e18206f1077e54f8b03af2833c
Timothy Henry <44324809+tmhvrml@users.noreply.github.com>
2023-03-31 18:39:33 -0400

    week2 update

---
+++ week2/form_demo02.php
@@ -0,0 +1,31 @@
+<?php
+    include_once('./controllers/frmDemo02func.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo Forms Data Capture</title>
+</head>
+<body>
+    <h1>Demo Forms</h1>
+    <form action="form_demo02.php" method="post">
+        Age: <input type="text" name="val1" value="Whatever" />
+        <input type="checkbox" value="Adult" name="adult" >Adult<br />
+        <input type="radio" value="single" name="status">Single
+        <input type="radio" value="relationship" name="status">In a relationship<br />
+        <select name="state">
+            <option>Rhode Island</option>
+            <option>Massachusetts</option>
+            <option selected>Connecticut</option>
+            <option>Maine</option>
+            <option>New Hampshire</option>
+            <option>Vermont</option>
+        </select>
+        
+        <input type="submit" name="submitBtn" />
+    </form>
+</body>
+</html>
---
+++ week2/postArray_demo01.php
@@ -0,0 +1,20 @@
+<?php
+    include_once('./controllers/postArrDemo01func.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms example</title>
+</head>
+<body>
+    <?php echo $error; ?>
+    <hr />
+    <form method="post" action="postArray_demo01.php">
+        Adult:<input type="checkbox" name="adult" value="yes" /><br />
+        <input type="text" value="<?php echo $firstName; ?>" name="firstName" />
+        <input type="submit" value="Submit This" name="submitThis" />
+    </form>   
+</body>
+</html>
