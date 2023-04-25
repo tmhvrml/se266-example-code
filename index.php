847e03fa5836dab089d1367d56d47adb20b4928b
tmhvrml <thenry@neit.edu>
2023-04-25 08:00:53 -0400

    index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <li><a href="week3/index.php">Week 3</a></li>
     <li><a href="week4/index.php">Week 4</a></li>
     <li><a href="week5/teams/index.php">Week 5</a></li>
-    <li><a href="week6r1/index.php">Week 6</a></li>
+    <li><a href="week6/index.php">Week 6</a></li>
     <li><a href="week7r1/index.php">Week 7</a></li>
     <li><a href="week8r1/index.php">Week 8</a></li>
     <li><a href="week9/index.php">Week 9</a></li>
